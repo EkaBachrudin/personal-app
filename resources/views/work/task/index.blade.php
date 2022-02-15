939eb7fac0c9724d00b3bef4b4b25820354590e5
Nursa <vvalentine825@gmail.com>
2022-02-15 14:36:36 +0700

    fixing task function

--- resources/views/work/task/index.blade.php
+++ resources/views/work/task/index.blade.php
@@ -2,13 +2,13 @@
 @section('firstTitle', 'Work')
 @section('title', 'task')
 @section('style')
-<meta   name="csrf-token" content="{{ csrf_token() }}">
-    <style>
-        .task-element{
-            display: inline-block;
-            margin-left: 10px
-        }
-    </style>
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<style>
+    .task-element {
+        display: inline-block;
+        margin-left: 10px
+    }
+</style>
 @endsection
 @section('content')
 <div class="container-fluid">
@@ -25,9 +25,11 @@
                 </div>
                 <h4 class="page-title">Task</h4>
                 <br>
-                <div class="btn btn-pink"  data-bs-toggle="modal" data-bs-target="#modalAdd"> Add task </div>
-            </div><!--end page-title-box-->
-        </div><!--end col-->
+                <div class="btn btn-pink" data-bs-toggle="modal" data-bs-target="#modalAdd"> Add task </div>
+            </div>
+            <!--end page-title-box-->
+        </div>
+        <!--end col-->
     </div>
     <!-- end page title end breadcrumb -->
 
@@ -35,35 +37,46 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-body">
-                    <div class="d-flex align-items-start">
+                    <div class="row d-flex align-items-start">
                         {{-- task from --}}
-                        <div class="nav flex-column nav-pills me-3 rounded shadow" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                          @foreach ($taskFroms as $taskFrom)
-                          <button class="nav-link {{$loop->first ? 'active' : ''}}" id="v-pills-{{$taskFrom->id}}-tab" data-bs-toggle="pill" data-bs-target="#v-pills-{{$taskFrom->id}}" type="button" role="tab" aria-controls="v-pills-{{$taskFrom->id}}" aria-selected="true">{{$taskFrom->name}}</button>
-                          @endforeach
-                        </div>
-                        {{-- end task from --}}
-                        <div class="tab-content" id="v-pills-tabContent">
-                        {{-- task --}}
-                          @foreach ($taskFroms as $taskFrom)
-                          <div class="tab-pane fade show {{$loop->first ? 'active' : ''}}" id="v-pills-{{$taskFrom->id}}" role="tabpanel" aria-labelledby="v-pills-{{$taskFrom->id}}-tab">
-                                @foreach ($taskFrom->task as $task)
-                                <div class="card shadow task-element">
-                                    <div class="card-body">
-                                        <div class="d-flex justify-content-between">
-                                            <h5>{{$task->title}}</h5>
-                                            <i class="fas fa-edit text-pink" onclick="edit({{$task->id}})"></i>
-                                        </div>
-                                        <hr>
-                                        <p>{{$task->task}}</p>
-                                    </div>
-                                </div>
+                        <div class="col-md-2">
+                            <div class="nav flex-column nav-pills me-3 rounded shadow" id="v-pills-tab" role="tablist"
+                                aria-orientation="vertical">
+                                @foreach ($taskFroms as $taskFrom)
+                                <button class="nav-link {{$loop->first ? 'active' : ''}}"
+                                    id="v-pills-{{$taskFrom->id}}-tab" data-bs-toggle="pill"
+                                    data-bs-target="#v-pills-{{$taskFrom->id}}" type="button" role="tab"
+                                    aria-controls="v-pills-{{$taskFrom->id}}"
+                                    aria-selected="true">{{$taskFrom->name}}</button>
                                 @endforeach
                             </div>
-                          @endforeach
-                        {{-- end task --}}
                         </div>
-                      </div>
+                        {{-- end task from --}}
+                        <div class="col-md-10">
+                            <div class="tab-content" id="v-pills-tabContent">
+                                {{-- task --}}
+                                @foreach ($taskFroms as $taskFrom)
+                                <div class="tab-pane fade show {{$loop->first ? 'active' : ''}}"
+                                    id="v-pills-{{$taskFrom->id}}" role="tabpanel"
+                                    aria-labelledby="v-pills-{{$taskFrom->id}}-tab">
+                                    @foreach ($taskFrom->task as $task)
+                                    <div class="card shadow task-element">
+                                        <div class="card-body">
+                                            <div class="d-flex justify-content-between">
+                                                <h5>{{$task->title}}</h5>
+                                                <i class="fas fa-edit text-pink" onclick="edit({{$task->id}})"></i>
+                                            </div>
+                                            <hr>
+                                            <textarea class="form-control" cols="30" rows="15" disabled> {{$task->task}} </textarea>
+                                        </div>
+                                    </div>
+                                    @endforeach
+                                </div>
+                                @endforeach
+                                {{-- end task --}}
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -84,65 +97,67 @@
         }
     });
 
-    function submitCreate(){
-        var submitTaskFrom   = $('#create').find( $('input[name="task-from"]') ).val();
-        var submitTitle      = $('#create').find( $('input[name="title"]') ).val();
-        var submitTask       = $('#create').find( $('textarea[name="task"]') ).val();
+    function submitCreate() {
+        var submitTaskFrom = $('#create').find($('input[name="task-from"]')).val();
+        var submitTitle = $('#create').find($('input[name="title"]')).val();
+        var submitTask = $('#create').find($('textarea[name="task"]')).val();
 
         var request = $.ajax({
-        url: "/task/index/create",
-        method: "POST",
-        data: { 
-            fromTask    : submitTaskFrom,
-            title       : submitTitle,
-            task        : submitTask
-        },
+            url: "/task/index/create",
+            method: "POST",
+            data: {
+                fromTask: submitTaskFrom,
+                title: submitTitle,
+                task: submitTask
+            },
         });
-        
-        request.done(function( data ) {
+
+        request.done(function (data) {
             alert(data.success);
+            location.reload();
         });
-        
-        request.fail(function( jqXHR, textStatus ) {
-        alert( "Request failed: " + textStatus );
+
+        request.fail(function (jqXHR, textStatus) {
+            alert("Request failed: " + textStatus);
         });
     };
 
-    function edit(id){
+    function edit(id) {
         $.ajax({
-            method: "GET",
-            url: "/task/index/getData/"+id,
-        })
-        .done(function( data ) {
-            $('#edit').find( $('input[name="task-from"]') ).val(data.taskFrom.name);
-            $('#edit').find( $('input[name="title"]') ).val(data.task.title);
-            $('#edit').find( $('textarea[name="task"]') ).val(data.task.task);
-            $('#edit').find( $('.btn-primary').attr('onclick', 'submitUpdate('+data.task.id+')') );
-            $('#modalEdit').modal('show');
-        });
+                method: "GET",
+                url: "/task/index/getData/" + id,
+            })
+            .done(function (data) {
+                $('#edit').find($('input[name="task-from"]')).val(data.taskFrom.name);
+                $('#edit').find($('input[name="title"]')).val(data.task.title);
+                $('#edit').find($('textarea[name="task"]')).val(data.task.task);
+                $('#edit').find($('.btn-primary').attr('onclick', 'submitUpdate(' + data.task.id + ')'));
+                $('#modalEdit').modal('show');
+            });
     };
 
-    function submitUpdate(id){
-        var submitTaskFrom   = $('#edit').find( $('input[name="task-from"]') ).val();
-        var submitTitle      = $('#edit').find( $('input[name="title"]') ).val();
-        var submitTask       = $('#edit').find( $('textarea[name="task"]') ).val();
+    function submitUpdate(id) {
+        var submitTaskFrom = $('#edit').find($('input[name="task-from"]')).val();
+        var submitTitle = $('#edit').find($('input[name="title"]')).val();
+        var submitTask = $('#edit').find($('textarea[name="task"]')).val();
 
         var request = $.ajax({
-        url: "/task/index/update/"+id,
-        method: "POST",
-        data: { 
-            fromTask    : submitTaskFrom,
-            title       : submitTitle,
-            task        : submitTask
-        },
+            url: "/task/index/update/" + id,
+            method: "POST",
+            data: {
+                fromTask: submitTaskFrom,
+                title: submitTitle,
+                task: submitTask
+            },
         });
-        
-        request.done(function( data ) {
+
+        request.done(function (data) {
             alert(data.success);
+            location.reload();
         });
-        
-        request.fail(function( jqXHR, textStatus ) {
-        alert( "Request failed: " + textStatus );
+
+        request.fail(function (jqXHR, textStatus) {
+            alert("Request failed: " + textStatus);
         });
     };
 </script>
